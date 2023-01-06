4866158f29dc2d2f80dd5f3fbf62c9e57c9f07c0
Yogendra <99913694+Yogendra-sodha@users.noreply.github.com>
2023-01-06 04:00:34 -0500

    Added solution for removing duplicates

--- Linked List/Remove-Dups-LL.py
+++ Linked List/Remove-Dups-LL.py
@@ -1,9 +1,24 @@
 from LL_Skeleton import LinkedListBody
 
-class Node:
+def removeDups(ll):
+    if ll.head is None:
+        return
 
-    def __init__(self,data):
-        
+    else:
+        temp = ll.head
+        uniSet = []
+        while temp.next:
+            if temp.next.data in uniSet:
+                temp.next = temp.next.next
+            else:
+                uniSet.append(temp.data)
+                temp = temp.next
+        return ll
 
+# Call class from linkedlist py file the body that was created
 
-def removeDup:
+customLL = LinkedListBody()
+customLL.generate(5,3,6)
+print(customLL)
+removeDups(customLL)
+print(customLL)
