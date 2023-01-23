6c75761f4307956f97f3c8f92a2be2f362e263c2
Yogendra <99913694+Yogendra-sodha@users.noreply.github.com>
2023-01-22 21:41:31 -0500

    ll ques

--- Linked List/LL_Skeleton.py
+++ Linked List/LL_Skeleton.py
@@ -1,52 +1,55 @@
 from random import randint
 class Node:
-    def __init__(self,data=None):
-        self.data = data
+    def __init__(self, value=None):
+        self.value = value
         self.next = None
         self.prev = None
-
+    
     def __str__(self):
-        return self.data
+        return str(self.value)
 
-class LinkedListBody:
-    def __init__(self):
+class LL:
+    def __init__(self, values = None):
         self.head = None
         self.tail = None
-    
+
     def __iter__(self):
-        node = self.head
-        while node:
-            yield node
-            node = node.next
+        curNode = self.head
+        while curNode:
+            yield curNode
+            curNode = curNode.next
     
     def __str__(self):
-        values = [str(x.data) for x in self]
-        return "->".join(values)
+        values = [str(x.value) for x in self]
+        return ' -> '.join(values)
     
     def __len__(self):
-        temp = self.head
         result = 0
-        while temp:
-            temp = temp.next
+        node = self.head
+        while node:
             result += 1
+            node = node.next
         return result
-
-    def add(self,value):
+    
+    def add(self, value):
         if self.head is None:
             newNode = Node(value)
             self.head = newNode
             self.tail = newNode
-
-        else :
-            # Adding node at to the tail of LL
-            self.tail.next = Node(value) # as tail next refernece would be null
-            self.tail = self.tail.next # now tail's next refernce is new tail so assigning new tail
-            return self.tail
-
-    def generate(self,n,min_range,max_range):
-        # Creating new node
+        else:
+            self.tail.next = Node(value)
+            self.tail = self.tail.next
+        return self.tail
+    
+    def generate(self, n, min_value, max_value):
         self.head = None
         self.tail = None
         for i in range(n):
-            self.add(randint(min_range,max_range))
+            self.add(randint(min_value,max_value))
         return self
+
+# customLL = LinkedList()
+# customLL.generate(10, 0, 99)
+# print(customLL)
+# print(len(customLL))
+
